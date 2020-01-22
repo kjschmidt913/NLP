67a1ab147566a18f143b085c669aaab42a645d8f
Aristana Scourtas <ascourtas@gmail.com>
2020-01-22 00:02:50 -0600

    get proper search params, do year logic

--- project-1/nytimes.py
+++ project-1/nytimes.py
@@ -49,14 +49,19 @@ api = articleAPI('qbQh29tAn22AwG80h8MfRUNxucW1kcTi')
 #     print(news)
 #     return(news)
 
-def get_articles(date_year,query):
+def get_articles(date_year,query, election_yr=2016, next_election_yr=2020):
     all_articles = []
     # max 100 pages returned
     for i in range(0,10):
         # returns 10 results per page
         articles = api.search(
             q = query,
-            fq = {'source': 'New York Times'},
+            # fq = {'body': 'US presidential election', 'headline': 'US presidential election', 'byline': 'US presidential election'},
+
+            fq = {'body': ["US", "presidential", "election"],
+                  'subject': ['Presidential Election of {}'.format(election_yr),
+                              'Presidential Election of {}'.format(next_election_yr),
+                              'Presidents and Presidency (US)']},
             begin_date = int(date_year + '0101'),
             end_date = int(date_year + '1231'),
             page = i
@@ -64,7 +69,12 @@ def get_articles(date_year,query):
         # need to sleep so we don't get locked out of NYT
         time.sleep(6)
         # TODO: remove, just for testing
-        all_articles.append(articles['response']['docs'])
+        if 'response' not in articles.keys():
+            if 'fault' in articles.keys():
+                print("Error from API: {}".format(articles['fault']['faultstring']))
+            continue
+        if len(articles['response']['docs']) > 0:
+            all_articles.extend(articles['response']['docs'])
 
         # for j in range(len(articles['response']['docs'])):
         #     article = articles['response']['docs'][j]['web_url']
@@ -74,16 +84,33 @@ def get_articles(date_year,query):
     return all_articles
 
 
-all = []
+all_articles = []
 # TODO: change year range
-# NOTE: doesn't like ES OR or AND, for some reason
-keyphrase = "(president) OR (election)"
-for i in range(2015, 2017):
-    year = get_articles(str(i), keyphrase)
-    all = all + year
+# NOTE: doesn't like ES OR or AND, due to library handling of url request format
+keyphrase = "election~"
+election_years = range(1968, 2020, 4) # step every 4 years
+# for i in range(2015, 2017):
+#     for j in range(0, 4):
+# year = get_articles(str(i), keyphrase, election_yr=election_years[i])
+# all = all + year
+for yr in election_years:
+    next_yr = yr + 4
+    search_yr = yr
+    for i in range(0, 4):
+        search_yr = yr + i
+        if search_yr < 1851:
+            continue
+
+        articles_for_year = get_articles(str(search_yr), keyphrase, election_yr=yr, next_election_yr=next_yr)
+        if len(articles_for_year) == 0:
+            print("No articles for year {}".format(search_yr))
+        else:
+            print("Articles for year {}:".format(search_yr))
+            print(articles_for_year)
+        all_articles = all_articles + articles_for_year
 
 with open('output.json', 'w+') as out:
-    out.write(all)
+    out.write(all_articles)
 
 
 # keys = all[0].keys()
