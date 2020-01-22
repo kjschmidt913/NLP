ad09edeec11d9cedbb2499f83df6522c3d9cc5a3
KJ <kj.schmidt913@gmail.com>
2020-01-22 15:17:54 -0600

    added patterns for tags

--- project-1/doc-tags.py
+++ project-1/doc-tags.py
@@ -1,10 +1,20 @@
+import nltk
 import re
+import pprint
+from nltk import word_tokenize
 
-#open the text file
-document = open("raw_text.txt","r")
+# open the text file
+document = open("raw_text.txt", "r")
 
 
-#this is for year
-expression = "\d\d\d\d$"
-replace = "<year>"
-docWithYearTags = regexprep(document,expression,replace)
+patterns = [
+    (r'.*ing$', 'VBG'),                # gerunds
+    (r'.*ed$', 'VBD'),                 # simple past
+    (r'.*es$', 'VBZ'),                 # 3rd singular present
+    (r'.*ould$', 'MD'),                # modals
+    (r'.*\'s$', 'NN$'),                # possessive nouns
+    (r'.*s$', 'NNS'),                  # plural nouns
+    (r'^-?[0-9]+(\.[0-9]+)?$', 'CD'),  # cardinal numbers
+    (r'.*', 'NN'),                      # nouns (default)
+    (r'^-?[0-9]+(\.[0-9]+)+(\.[0-9]+)+(\.[0-9]+)?$', 'YY') # year (default)
+]
