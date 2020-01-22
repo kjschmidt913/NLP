f2b5d4e6c19ea2c813cb9397975d643b854126b2
Aristana Scourtas <ascourtas@gmail.com>
2020-01-22 00:16:03 -0600

    clean up and add comments

--- project-1/nytimes.py
+++ project-1/nytimes.py
@@ -1,4 +1,4 @@
-from NYTimesArticleAPI import articleAPI
+from NYTimesArticleAPI import articleAPI  # need to use local, manually copied package for now
 import pprint
 import json
 import time
@@ -49,15 +49,24 @@ api = articleAPI('qbQh29tAn22AwG80h8MfRUNxucW1kcTi')
 #     print(news)
 #     return(news)
 
-def get_articles(date_year,query, election_yr=2016, next_election_yr=2020):
+def get_articles(date_year, query, election_yr=2016, next_election_yr=2020):
+    """
+    Return article URLs about the given elections for the given date year.
+
+    The multiple 'subject' values appear to be OR'd together -- you don't need all of them to match.
+
+    :param date_year: The year to search for news in, from Jan 1 to Dec 31
+    :param query: The singular primary query term
+    :param election_yr: The election year's news to search for
+    :param next_election_yr: The following election year
+    :return all_articles: All articles across 10 pages (max 100) for the given search year
+    """
     all_articles = []
-    # max 100 pages returned
-    for i in range(0,10):
+    page_range = range(0, 10)  # max 99
+    for i in page_range:
         # returns 10 results per page
         articles = api.search(
             q = query,
-            # fq = {'body': 'US presidential election', 'headline': 'US presidential election', 'byline': 'US presidential election'},
-
             fq = {'body': ["US", "presidential", "election"],
                   'subject': ['Presidential Election of {}'.format(election_yr),
                               'Presidential Election of {}'.format(next_election_yr),
@@ -68,48 +77,53 @@ def get_articles(date_year,query, election_yr=2016, next_election_yr=2020):
             )
         # need to sleep so we don't get locked out of NYT
         time.sleep(6)
-        # TODO: remove, just for testing
+
+        # this means we probably hit the NYT ping quota -- skip ahead
         if 'response' not in articles.keys():
             if 'fault' in articles.keys():
                 print("Error from API: {}".format(articles['fault']['faultstring']))
             continue
-        if len(articles['response']['docs']) > 0:
-            all_articles.extend(articles['response']['docs'])
 
-        # for j in range(len(articles['response']['docs'])):
-        #     article = articles['response']['docs'][j]['web_url']
-        #     print(article)
-        #     all_articles.append(article)
-    print(all_articles)
+        if len(articles['response']['docs']) > 0:
+            for j in range(len(articles['response']['docs'])):
+                article = articles['response']['docs'][j]['web_url']
+                # print(article)
+                all_articles.append(article)
+
+    # print(all_articles)
     return all_articles
 
 
 all_articles = []
-# TODO: change year range
-# NOTE: doesn't like ES OR or AND, due to library handling of url request format
+
+# NOTE: doesn't like ES OR or AND, due to bug in library handling of url request format
 keyphrase = "election~"
-election_years = range(1968, 2020, 4) # step every 4 years
-# for i in range(2015, 2017):
-#     for j in range(0, 4):
-# year = get_articles(str(i), keyphrase, election_yr=election_years[i])
-# all = all + year
+# start in 1968 cuz we don't get data with our 'fq' params before then
+election_years = range(1968, 2016, 4) # step every 4 years
+
 for yr in election_years:
+    # the following election year
     next_yr = yr + 4
+    # the current year we're searching for news in
     search_yr = yr
+    # 0-4 for each year between election years
     for i in range(0, 4):
         search_yr = yr + i
-        if search_yr < 1851:
+        if search_yr < 1851:  # NYT data doesn't go before 1851
             continue
-
         articles_for_year = get_articles(str(search_yr), keyphrase, election_yr=yr, next_election_yr=next_yr)
+
+        # print intermediate results to console
         if len(articles_for_year) == 0:
             print("No articles for year {}".format(search_yr))
         else:
             print("Articles for year {}:".format(search_yr))
             print(articles_for_year)
+            
         all_articles = all_articles + articles_for_year
 
-with open('output.json', 'w+') as out:
+# print final results to file
+with open('output.txt', 'w+') as out:
     out.write(all_articles)
 
 
