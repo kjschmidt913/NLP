cbc7b93267288d4c47a42127d075dbae763ae852
Neelanshi Varia <neelanshiV2@gmail.com>
2020-01-22 15:33:03 -0600

    Tokenise and CLEAN clean the file

--- project-1/cleanup.py
+++ project-1/cleanup.py
@@ -1,25 +1,51 @@
+from nltk import tokenize
+import nltk
+import re
+nltk.download('punkt')
+
+#remove quotes resulting from <p> elements of text scraping
 fin = open("raw_text.txt", "rt")
 fout = open("out.txt", "wt")
 
 for line in fin:
-	fout.write(line.replace('\'\'', ' '))
+	fout.write(line.replace('\'\'', ''))
 
 fin.close()
 fout.close()
 
-import re
+#replace all the punctuations except .?!,:; with @-@
 original_string = open('out.txt').read()
-new_string = re.sub('[ ](?=[ ])|[^,A-Za-z0-9 .?!;:]+', '@-@', original_string)
+new_string = re.sub('[ ](?=[ ])|[^-_,A-Za-z0-9 .?!,:;]+', '@-@', original_string)
 open('out2.txt', 'w').write(new_string)
 
+#add spaces between sentences which don't have a space between period and capital word due to the fetching pattern
 fin = open("out2.txt", "rt")
-fout = open("out3.txt", "wt")
-
+fout = open("out.txt", "wt")
 for line in fin:
-    fout.write(line.replace('. ', '</s> <s>'))
-
+    fout.write(re.sub('([.])([A-Z])', r'\1 \2', line))
 fin.close()
 fout.close()
 
+#tokenise and add <s> </s> tags
+fin = open("out.txt", "rt")
+fout = open("out2.txt", "wt")
+for line in fin:
+    y = tokenize.sent_tokenize(line)
 
-#would be great to find a better of doing it without multiple files
+for line in y:
+    fout.write('<s> ')
+    fout.write(line)
+    fout.write(' </s> ')
+fin.close()
+fout.close()
+
+#remove double hyphens with
+fin = open("out2.txt", "rt")
+fout = open("out_final.txt", "wt")
+
+for line in fin:
+    fout.write(line.replace('--', ''))
+fin.close()
+fout.close()
+
+#final output file to consider is out_final.txt
