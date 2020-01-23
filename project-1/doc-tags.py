2fe65bd4bdf2481d88a39f0e919ba28b104201f3
KJ <kj.schmidt913@gmail.com>
2020-01-22 21:16:49 -0600

    just copy and pasted vocabulary class as a fix until file is added to master:

--- project-1/doc-tags.py
+++ project-1/doc-tags.py
@@ -6,7 +6,7 @@ import nltk
 # 1. uncomment the actual corpus and remove dummy corpus
 # 2. Do stats for patterns - import Vocabulary class once Nayan merges her branch
 
-def doc_tagging (textfile):
+def doc_tagging (textfile, type):
 
     # This is solely for testing. Remove once using real corpus
     stringthing = "Hello welcome to the world of to learn Categorizing and POS Tagging with NLTK and Python this should be a yeah 1992 and this a cardinal number 0.4"
@@ -51,6 +51,52 @@ def doc_tagging (textfile):
 
     print(new_tokens)
 
+    voc = Vocabulary(type)
+
+    for sent in new_tokens:
+        voc.add_sentence(sent)
+
+    print(voc.index2word)
+    for word in range(voc.num_words):
+        print(word," ",voc.to_word(word))
+
+SOS_token = 0
+EOS_token = 1
+
+
+class Vocabulary:
+
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {SOS_token: "SOS", EOS_token: "EOS"}
+        self.num_words = 2
+        self.num_sentences = 0
+
+    def add_word(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.num_words
+            self.word2count[word] = 1
+            self.index2word[self.num_words] = word
+            self.num_words += 1
+        else:
+            self.word2count[word] += 1
+
+    def add_sentence(self, sentence):
+        sentence_len = 0
+        for word in sentence.split(' '):
+            sentence_len += 1
+            self.add_word(word)
+        self.num_sentences += 1
+
+    def to_word(self, index):
+        return self.index2word[index]
+
+    def to_index(self, word):
+        return self.word2index[word]
+
+
 
 
 # UNCOMMENT TO USE ACTUAL CORPUS
@@ -58,6 +104,6 @@ def doc_tagging (textfile):
 # doc_tagging("group3_train.txt")
 # doc_tagging("group3_valid.txt")
 
-doc_tagging("hi")
+doc_tagging("hi", "Train")
 
 
