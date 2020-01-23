b2f38a5c3523e887b105e0ae7e0bc7b1435a65f0
KJ <kj.schmidt913@gmail.com>
2020-01-22 21:20:14 -0600

    added the vocabulary class

--- project-1/doc-tags.py
+++ project-1/doc-tags.py
@@ -1,4 +1,5 @@
 import nltk
+from construct_vocab import Vocabulary
 
 
 
@@ -59,42 +60,6 @@ def doc_tagging (textfile, type):
     print(voc.index2word)
     for word in range(voc.num_words):
         print(word," ",voc.to_word(word))
-
-SOS_token = 0
-EOS_token = 1
-
-
-class Vocabulary:
-
-    def __init__(self, name):
-        self.name = name
-        self.word2index = {}
-        self.word2count = {}
-        self.index2word = {SOS_token: "SOS", EOS_token: "EOS"}
-        self.num_words = 2
-        self.num_sentences = 0
-
-    def add_word(self, word):
-        if word not in self.word2index:
-            self.word2index[word] = self.num_words
-            self.word2count[word] = 1
-            self.index2word[self.num_words] = word
-            self.num_words += 1
-        else:
-            self.word2count[word] += 1
-
-    def add_sentence(self, sentence):
-        sentence_len = 0
-        for word in sentence.split(' '):
-            sentence_len += 1
-            self.add_word(word)
-        self.num_sentences += 1
-
-    def to_word(self, index):
-        return self.index2word[index]
-
-    def to_index(self, word):
-        return self.word2index[word]
 
 
 
