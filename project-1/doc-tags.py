3a595b5c98873ee6dea23a96513b3e90ed1c42ee
KJ <kj.schmidt913@gmail.com>
2020-01-22 21:40:54 -0600

    updated doc tags to include token and vocab counts and to pull from actual corpus

--- project-1/doc-tags.py
+++ project-1/doc-tags.py
@@ -9,11 +9,11 @@ from construct_vocab import Vocabulary
 def doc_tagging (textfile, train_test_valid):
 
     # This is solely for testing. Remove once using real corpus
-    stringthing = "Hello welcome to the world of to learn Categorizing and POS Tagging with NLTK and Python this should be a yeah 1992 and this a cardinal number 0.4"
-    text = nltk.word_tokenize(stringthing)
+    # stringthing = "Hello welcome to the world of to learn Categorizing and POS Tagging with NLTK and Python this should be a yeah 1992 and this a cardinal number 0.4"
+    # text = nltk.word_tokenize(stringthing)
 
-    # file_content = open(textfile).read()
-    # text = nltk.word_tokenize(file_content)
+    file_content = open(textfile).read()
+    text = nltk.word_tokenize(file_content)
 
     patterns = [
         (r'^(19|20)\d\d$', 'YY'),  # years
@@ -57,17 +57,23 @@ def doc_tagging (textfile, train_test_valid):
         voc.add_sentence(sent)
 
     print(voc.index2word)
+
+    vocab_count = 0
     for word in range(voc.num_words):
+        vocab_count += 1
         print(word," ",voc.to_word(word))
+
+    print("Vocab Count: ", vocab_count)
+    print("Token Count: ", len(new_tokens))
 
 
 
 
 # UNCOMMENT TO USE ACTUAL CORPUS
-# doc_tagging("group3_test.txt")
-# doc_tagging("group3_train.txt")
-# doc_tagging("group3_valid.txt")
+doc_tagging("group3_test.txt")
+doc_tagging("group3_train.txt")
+doc_tagging("group3_valid.txt")
 
-doc_tagging("hi", "Train")
+# doc_tagging("hi", "Train")
 
 
